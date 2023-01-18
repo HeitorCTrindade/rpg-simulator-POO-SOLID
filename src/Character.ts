4658c6e00de24731da23301ad86435ecfd755aaa
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-01-18 17:49:38 -0300

    Requisito 07 - implementando - Classe Character em construção.

---
+++ src/Character.ts
@@ -0,0 +1,33 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+    this._dexterity = (Math.floor(Math.random() * 10));
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = (this._race.maxLifePoints / 2);
+    this.li
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }  
+
+  static createdArchetypeInstances() {
+    return this.instances;
+  }
+}
