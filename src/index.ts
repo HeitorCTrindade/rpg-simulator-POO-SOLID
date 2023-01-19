b20ced521bf00164f6abd95dd0cdeb80be2778b2
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-01-19 18:54:32 -0300

    Requisito 13 - bonus - implementado.

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const LEVEL_20 = 20;
+
+const player1 = new Character('Zezin');
+const player2 = new Character('Doidin');
+const player3 = new Character('Shaolin Matador de Porco');
+
+for (let i = 0; i < LEVEL_20; i += 1) player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]) {
+  battles.forEach((battle) => battle.fight());
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles }; 
